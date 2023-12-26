90ca6a52bc564ba705d5e45737986b62e4906796
elliot <elliot.rourke@gmail.com>
2023-12-26 23:24:01 +0000

    Updating corner/side clauses

--- src/main/java/elliot/aoc/gondola/NearIntegerDetector.java
+++ src/main/java/elliot/aoc/gondola/NearIntegerDetector.java
@@ -4,29 +4,94 @@ import static elliot.aoc.util.Constant.MATRIX_DEPTH;
 
 public class NearIntegerDetector {
 
+    private static final String IS_NUMBER_REGEX = "[0-9]";
+
     private NearIntegerDetector() {
     }
 
     public static boolean hasNearIntegers(String[][] matrix, MatrixIndex index) {
         //Need to check in all directions around point, but obey constraints (sides, corners)
         if (isTopRightCorner(matrix, index)) {
-            return false;
+            String left = matrix[index.getLength()][index.getDepth() - 1];
+            String below = matrix[index.getLength() + 1][index.getDepth()];
+            String diagonal = matrix[index.getLength() + 1][index.getDepth() - 1];
+            return left.matches(IS_NUMBER_REGEX) || below.matches(IS_NUMBER_REGEX) || diagonal.matches(IS_NUMBER_REGEX);
         } else if (isTopLeftCorner(matrix, index)) {
-            return false;
+            String right = matrix[index.getLength()][index.getDepth() + 1];
+            String below = matrix[index.getLength() + 1][index.getDepth()];
+            String diagonal = matrix[index.getLength() + 1][index.getDepth() + 1];
+            return right.matches(IS_NUMBER_REGEX) || below.matches(IS_NUMBER_REGEX) || diagonal.matches(IS_NUMBER_REGEX);
         } else if (isBottomRightCorner(matrix, index)) {
-            return false;
+            String left = matrix[index.getLength()][index.getDepth() - 1];
+            String above = matrix[index.getLength() - 1][index.getDepth()];
+            String diagonal = matrix[index.getLength() - 1][index.getDepth() - 1];
+            return left.matches(IS_NUMBER_REGEX) || above.matches(IS_NUMBER_REGEX) || diagonal.matches(IS_NUMBER_REGEX);
         } else if (isBottomLeftCorner(matrix, index)) {
-            return false;
+            String right = matrix[index.getLength()][index.getDepth() + 1];
+            String above = matrix[index.getLength() - 1][index.getDepth()];
+            String diagonal = matrix[index.getLength() - 1][index.getDepth() + 1];
+            return right.matches(IS_NUMBER_REGEX) || above.matches(IS_NUMBER_REGEX) || diagonal.matches(IS_NUMBER_REGEX);
         } if (isLeftSide(matrix, index)) {
-            return false;
+            String right = matrix[index.getLength()][index.getDepth() + 1];
+            String above = matrix[index.getLength() - 1][index.getDepth()];
+            String below = matrix[index.getLength() + 1][index.getDepth()];
+            String diagonalAbove = matrix[index.getLength() - 1][index.getDepth() + 1];
+            String diagonalBelow = matrix[index.getLength() + 1][index.getDepth() + 1];
+            return right.matches(IS_NUMBER_REGEX)
+                    || above.matches(IS_NUMBER_REGEX)
+                    || below.matches(IS_NUMBER_REGEX)
+                    || diagonalAbove.matches(IS_NUMBER_REGEX)
+                    || diagonalBelow.matches(IS_NUMBER_REGEX);
         } else if (isRightSide(matrix, index)) {
-            return false;
+            String left = matrix[index.getLength()][index.getDepth() - 1];
+            String above = matrix[index.getLength() - 1][index.getDepth()];
+            String below = matrix[index.getLength() + 1][index.getDepth()];
+            String diagonalAbove = matrix[index.getLength() - 1][index.getDepth() - 1];
+            String diagonalBelow = matrix[index.getLength() + 1][index.getDepth() - 1];
+            return left.matches(IS_NUMBER_REGEX)
+                    || above.matches(IS_NUMBER_REGEX)
+                    || below.matches(IS_NUMBER_REGEX)
+                    || diagonalAbove.matches(IS_NUMBER_REGEX)
+                    || diagonalBelow.matches(IS_NUMBER_REGEX);
         } else if (isBottomSide(matrix, index)) {
-            return false;
+            String left = matrix[index.getLength()][index.getDepth() - 1];
+            String right = matrix[index.getLength()][index.getDepth() + 1];
+            String above = matrix[index.getLength() - 1][index.getDepth()];
+            String diagonalLeft = matrix[index.getLength() - 1][index.getDepth() - 1];
+            String diagonalRight = matrix[index.getLength() - 1][index.getDepth() + 1];
+            return left.matches(IS_NUMBER_REGEX)
+                    || right.matches(IS_NUMBER_REGEX)
+                    || above.matches(IS_NUMBER_REGEX)
+                    || diagonalLeft.matches(IS_NUMBER_REGEX)
+                    || diagonalRight.matches(IS_NUMBER_REGEX);
         } else if (isTopSide(matrix, index)) {
-            return false;
+            String left = matrix[index.getLength()][index.getDepth() - 1];
+            String right = matrix[index.getLength()][index.getDepth() + 1];
+            String below = matrix[index.getLength() + 1][index.getDepth()];
+            String diagonalLeft = matrix[index.getLength() - 1][index.getDepth() - 1];
+            String diagonalRight = matrix[index.getLength() - 1][index.getDepth() + 1];
+            return left.matches(IS_NUMBER_REGEX)
+                    || right.matches(IS_NUMBER_REGEX)
+                    || below.matches(IS_NUMBER_REGEX)
+                    || diagonalLeft.matches(IS_NUMBER_REGEX)
+                    || diagonalRight.matches(IS_NUMBER_REGEX);
         } else {
-            return false;
+            String left = matrix[index.getLength()][index.getDepth() - 1];
+            String right = matrix[index.getLength()][index.getDepth() + 1];
+            String above = matrix[index.getLength() - 1][index.getDepth()];
+            String below = matrix[index.getLength() + 1][index.getDepth()];
+            String diagonalLeftAbove = matrix[index.getLength() - 1][index.getDepth() - 1];
+            String diagonalRightAbove = matrix[index.getLength() - 1][index.getDepth() + 1];
+            String diagonalLeftBelow = matrix[index.getLength() + 1][index.getDepth() - 1];
+            String diagonalRightBelow = matrix[index.getLength() + 1][index.getDepth() + 1];
+            return left.matches(IS_NUMBER_REGEX)
+                    || right.matches(IS_NUMBER_REGEX)
+                    || above.matches(IS_NUMBER_REGEX)
+                    || below.matches(IS_NUMBER_REGEX)
+                    || diagonalLeftAbove.matches(IS_NUMBER_REGEX)
+                    || diagonalRightAbove.matches(IS_NUMBER_REGEX)
+                    || diagonalLeftBelow.matches(IS_NUMBER_REGEX)
+                    || diagonalRightBelow.matches(IS_NUMBER_REGEX);
         }
     }
 
