99c96e1c736167b0735863f05a8b23b93b430b15
elliot <elliot.rourke@gmail.com>
2023-12-26 21:14:53 +0000

    Adding details to NearIntegerDetector

--- src/main/java/elliot/aoc/gondola/NearIntegerDetector.java
+++ src/main/java/elliot/aoc/gondola/NearIntegerDetector.java
@@ -6,8 +6,20 @@ public class NearIntegerDetector {
 
     public static boolean hasNearIntegers(String[][] matrix, MatrixIndex index) {
         //Need to check in all directions around point, but obey constraints (sides, corners)
+        if (isIndexASidePoint(matrix, index)) {
+            return false;
+        } else if (isIndexACornerPoint(matrix, index)) {
+            return false;
+        } else {
+            return false;
+        }
+    }
 
+    private static boolean isIndexASidePoint(String[][] matrix, MatrixIndex index) {
+        return false;
+    }
 
+    private static boolean isIndexACornerPoint(String[][] matrix, MatrixIndex index) {
         return false;
     }
 }
